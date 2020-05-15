70228dee8232209e40faca7901113870f2bdf4d8
Taja House <ahlliahtaja@yahoo.com>
2020-05-14 18:24:31 -0700

    Ran test test and passed

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,9 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+it('renders without crashing', () => {
+  render(
+    <span className="greet">hello world</span>
+  );
+});
